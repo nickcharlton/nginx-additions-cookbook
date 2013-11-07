1657cfa71cf6c2ff1e314ab79d0b509dbee58782
Nick Charlton <hello@nickcharlton.net>
2013-11-07 16:24:10 +0000

    Implements the ability to dynamically render error pages.

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,3 +26,32 @@ template "/etc/nginx/sites-available/default" do
   
   notifies :reload, 'service[nginx]'
 end
+
+# generate the error pages
+error_pages = [
+  {
+    :code => 404,
+    :name => '404 Not Found',
+    :explanation => 'Sorry, that page couldn\'t be found.',
+    :message => 'It\'s possible that you were linked to the wrong place, or '\
+      'something has been configured incorrectly. If you need help, email the '\
+      'address below.'
+  }
+]
+
+error_pages.each do |page|
+  template "/var/www/#{page[:code]}.html" do
+    source  'error.erb'
+    owner   'www-data'
+    group   'www-data'
+    mode    '00640'
+
+    variables(
+      :name => page[:name],
+      :explanation => page[:explanation],
+      :message => page[:message],
+      :support_email => 'ops@nullgrid.net',
+      :status_page => 'status.nullgrid.net'
+    )
+  end
+end
