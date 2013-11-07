b6fc30a4615fdae0ae53d1dd393a7dfd3d064b7f
Nick Charlton <hello@nickcharlton.net>
2013-11-07 13:00:41 +0000

    Bumps the version to 1.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nick@nickcharlton.net'
 license          'MIT'
 description      'Configures a collection of additions to the default nginx cookbook.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 # it follows Debian convention, so let's be explicit
 supports 'ubuntu'
